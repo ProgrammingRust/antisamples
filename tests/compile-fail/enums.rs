31b5389a47a64d68a7d0c5dc2f585f4e795d78df
Jason Orendorff <jason.orendorff@gmail.com>
2016-02-19 18:18:09 -0600

    negative test for accessing fields of enums

---
+++ tests/compile-fail/enums.rs
@@ -0,0 +1,13 @@
+struct Point3d(f32, f32, f32);
+const ORIGIN: Point3d = Point3d(0.0, 0.0, 0.0);
+
+enum Shape {
+    Sphere { center: Point3d, radius: f32 },
+    Box { point1: Point3d, point2: Point3d }
+}
+
+fn main() {
+    let shape = Shape::Sphere { center: ORIGIN, radius: 1.0 };
+    let r = shape.radius;  // error: no field with that name
+    //~^ERROR: attempted access of field `radius` on type `Shape`, but no field with that name was found
+}
