475ad4c28f500b0d49407adc61a64fb0f9695d26
Jason Orendorff <jason.orendorff@gmail.com>
2016-02-22 12:18:50 -0600

    Add "privacy for extensibility" note to outline

---
+++ tests/compile-fail/enums_cant_use_variables_in_pattern.rs
@@ -0,0 +1,19 @@
+// error-pattern: unreachable pattern
+
+fn print_if_equal(x: i32, y: i32) {
+    match x {
+        y =>  // trying to match only if x == y
+            // (it doesn't work: see explanation below)
+            println!("{} == {}", x, y),
+        _ =>  // error: unreachable pattern
+            println!("not equal")
+    }
+}
+
+// This fails because identifiers introduce *new* bindings.
+// The pattern `y` here creates a new local variable `y`,
+// shadowing the argument `y`.
+
+fn main() {
+    print_if_equal(2, 4);
+}
