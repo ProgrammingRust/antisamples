266139aa2c379dc3bc28ebc8db73bcab8cdc966d
Jason Orendorff <jason.orendorff@gmail.com>
2016-03-26 11:17:49 -0500

    Get antisamples tests passing again. I don't understand compiletest (sigh).

--- tests/compile-fail/types_vectors_moved.rs
+++ tests/compile-fail/types_vectors_moved.rs
@@ -1,11 +1,12 @@
 // Copy of samples/examples/languages.rs, but with an extra println! added.
 // The book claims it'll fail with a particular message.
 
+// error-pattern: use of moved value: `languages`
+
 fn main() {
     // Get our command-line arguments as a vector of Strings.
     let languages : Vec<String> = std::env::args().skip(1).collect();
     for l in languages {
-    //~^ NOTE: `languages` moved here because it has type `collections::vec::Vec<collections::string::String>`, which is non-copyable
         println!("{}: {}", l,
                  if l.len() % 2 == 0 {
                      "functional"
@@ -14,5 +15,4 @@ fn main() {
                  });
     }
     println!("{} languages classified", languages.len());
-    //~^ ERROR: use of moved value: `languages`
 }
