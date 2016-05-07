1380330b4728c6465364728a839f6f12bf0a39e0
Jason Orendorff <jason.orendorff@gmail.com>
2016-05-07 14:04:43 -0500

    Update test corresponding to traits.md, which has to be updated since `AddAssign` was stabilized

--- tests/compile-fail/traits_generic_sum_1.rs
+++ tests/compile-fail/traits_generic_sum_1.rs
@@ -1,12 +1,12 @@
 // Attempt 1: Insufficient bounds on N.
 //
 // error-pattern: mismatched types
-// error-pattern: binary assignment operation `+=` cannot be applied to type `N`
+// error-pattern: binary operation `+` cannot be applied to type `N`
 
 fn sum<N>(values: &Vec<N>) -> N {
     let mut total: N = 0;
     for v in values {
-        total += *v;
+        total = total + *v;
     }
     total
 }
