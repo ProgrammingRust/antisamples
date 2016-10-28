6a9217a14fa9c6bf8759680c3843fa970a425398
Jason Orendorff <jason.orendorff@gmail.com>
2016-10-28 18:30:03 -0500

    Copy a neat trick from how rust-clippy uses compiletest. Now you can run a single antisamples test by doing `cd antisamples; TESTNAME=enums_fields cargo test`.

--- tests/runtests.rs
+++ tests/runtests.rs
@@ -1,11 +1,17 @@
 extern crate compiletest_rs as compiletest;
 use compiletest::common::Mode;
 use std::path::PathBuf;
+use std::env;
 
 fn run_tests(mode: Mode, dir: &str) {
     let mut config = compiletest::default_config();
     config.mode = mode;
     config.src_base = PathBuf::from(dir);
+    if let Ok(name) = env::var::<&str>("TESTNAME") {
+        let s: String = name.to_owned();
+        config.filter = Some(s)
+    }
+
     compiletest::run_tests(&config);
 }
 
