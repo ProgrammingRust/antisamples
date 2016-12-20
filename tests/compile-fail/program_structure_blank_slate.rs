717710ea618a041caae86a1fb7feda79a19b0055
Jason Orendorff <jason.orendorff@gmail.com>
2016-12-19 21:32:54 -0600

    tests/program-structure: The "proteins" example was broken.
    It wasn't under test, even though it's in the book. Ugh. Had to make
    changes to the book.

--- tests/compile-fail/program_structure_blank_slate.rs
+++ tests/compile-fail/program_structure_blank_slate.rs
@@ -1,17 +1,18 @@
-enum AminoAcid {
-    Ala, Arg, Asn, Asp, Cys, Gln, Glu, Gly, His, Ile,
-    Leu, Lys, Met, Phe, Pro, Ser, Thr, Trp, Tyr, Val
-}
-
 mod proteins {
-    // proteins.rs
-    pub fn synthesize(seq: &[AminoAcid])  // error: AminoAcid is undefined
-    //~^ ERROR: type name `AminoAcid` is undefined
-        -> Protein
-    {
-        Protein
+    enum AminoAcid {
+        Ala, Arg, Asn, Asp, Cys, Gln, Glu, Gly, His, Ile,
+        Leu, Lys, Met, Phe, Pro, Ser, Thr, Trp, Tyr, Val
     }
 
-    pub struct Protein;
-}
+    mod synthesis {
+        // proteins/synthesis.rs
+        pub fn synthesize(seq: &[AminoAcid])  // error: AminoAcid is undefined
+        //~^ ERROR: type name `AminoAcid` is undefined
+            -> Protein
+        {
+            Protein
+        }
 
+        pub struct Protein;
+    }
+}
