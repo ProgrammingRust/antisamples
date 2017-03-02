56cd34e77e554c1f56e34f39ad1df08018ab29a5
Jason Orendorff <jason.orendorff@gmail.com>
2017-03-02 13:57:42 -0600

    Change many tests to match recently changed text.

--- tests/compile-fail/traits_print_without_bound.rs
+++ tests/compile-fail/traits_print_without_bound.rs
@@ -6,7 +6,9 @@
 // error-pattern: the trait bound `<I as std::iter::Iterator>::Item: std::fmt::Display` is not satisfied
 
 /// Print out all the values produced by an iterator
-fn dump<I: Iterator>(iter: I) {
+fn dump<I>(iter: I)
+    where I: Iterator
+{
     let mut index: usize = 0;
     for value in iter {
         println!("{}: {}", index, value);   // error
