35c48838923b0ba38926a5206d688f0b84d96bf2
Jason Orendorff <jason.orendorff@gmail.com>
2017-03-13 12:36:28 -0500

    New tests for text on patterns.

--- tests/compile-fail/enums_cant_use_variables_in_pattern.rs
+++ tests/compile-fail/enums_cant_use_variables_in_pattern.rs
@@ -1,20 +1,35 @@
 // error-pattern: unreachable pattern
 #![deny(unreachable_patterns)]
 
-fn print_if_equal(x: i32, y: i32) {
-    match x {
-        y =>  // trying to match only if x == y
-              // (it doesn't work: see explanation below)
-            println!("{} == {}", x, y),
-        _ =>  // warning: unreachable pattern
-            println!("not equal")
+mod game {
+    pub type Error = &'static str;
+    pub type Result<T> = ::std::result::Result<T, Error>;
+}
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+struct Point(i32, i32);
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+struct Hex(i32, i32);
+
+fn point_to_hex(Point(x, y): Point) -> Option<Hex> {
+    Some(Hex(x, y))
+}
+
+fn check_move(current_hex: Hex, click: Point) -> game::Result<Hex> {
+    match point_to_hex(click) {
+        None =>
+            Err("That's not a game space."),
+        Some(current_hex) =>  // try to match if user clicked the current_hex
+                              // (it doesn't work: see explanation below)
+            Err("You are already there! You must click somewhere else."),
+        Some(other_hex) =>
+            Ok(other_hex)
     }
 }
 
-// This fails because identifiers introduce *new* bindings.
-// The pattern `y` here creates a new local variable `y`,
-// shadowing the argument `y`.
+// This fails because identifiers introduce *new* bindings...
 
 fn main() {
-    print_if_equal(2, 4);
+    let _ = check_move(Hex(3, 3), Point(0, 9));
 }
