5bb99ef713d6d2ee7fa657db011c84372edfd153
Jason Orendorff <jason.orendorff@gmail.com>
2017-04-05 10:47:00 -0500

    Update antisample for a changed error message.

--- tests/compile-fail/references_static_wat.rs
+++ tests/compile-fail/references_static_wat.rs
@@ -1,4 +1,4 @@
-// error-pattern: expected one of `!`, `(`, `+`, `::`, `<`, or `=`, found `;`
+// error-pattern: expected one of `!`, `(`, `::`, `<`, or `=`, found `;`
 
 // This code has several problems, and doesn't compile.
 static mut STASH: &i32;
