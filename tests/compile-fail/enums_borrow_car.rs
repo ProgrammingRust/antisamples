df9c0c92ab91158594e8d319d783d64be56bc833
Jason Orendorff <jason.orendorff@gmail.com>
2017-05-12 13:06:19 -0500

    New tests for revisions to the "Enums and patterns" chapter.
    Coverage of `&` in patterns changed, and `BinaryTree` was revised to use
    named fields.

---
+++ tests/compile-fail/enums_borrow_car.rs
@@ -0,0 +1,37 @@
+// You can't borrow a car and then scrap it for parts.
+
+// error-pattern: cannot move out of borrowed content
+
+struct Engine {}
+
+struct Car {
+    engine: Engine
+}
+
+static MORTIS: Car = Car {
+    engine: Engine {}
+};
+
+struct Friend {}
+
+impl Friend {
+    fn borrow_car(&self) -> Option<&'static Car> {
+        Some(&MORTIS)
+    }
+}
+
+fn sell<T>(item: T) {}
+fn go_to_vegas() {}
+
+fn main() {
+    let friend = Friend {};
+
+    match friend.borrow_car() {
+        Some(&Car { engine, .. }) =>  // error: can't move out of borrow
+            {
+                sell(engine);
+                go_to_vegas();
+            }
+        None => {}
+    }
+}
