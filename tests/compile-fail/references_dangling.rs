c1020ae2fc917a13b9464bbd954667b057d4a585
Jason Orendorff <jason.orendorff@gmail.com>
2017-05-30 16:58:50 -0500

    Change test to match text, which includes extra braces.
    The braces help make clear where the lifetimes end; they're shown in the
    error messages in the book.

--- tests/compile-fail/references_dangling.rs
+++ tests/compile-fail/references_dangling.rs
@@ -1,10 +1,12 @@
 // error-pattern: `x` does not live long enough
 
 fn main() {
-    let r;
     {
-        let x = 1;
-        r = &x;
+        let r;
+        {
+            let x = 1;
+            r = &x;
+        }
+        assert_eq!(*r, 1);  // bad: reads memory `x` used to occupy
     }
-    assert_eq!(*r, 1);  // bad: reads memory `x` used to occupy
 }
