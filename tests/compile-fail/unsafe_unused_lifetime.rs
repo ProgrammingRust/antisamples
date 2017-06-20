cfd35296e11bf9f3a46489dcf853bde892ed30bb
Jim Blandy <jimb@red-bean.com>
2017-06-20 14:38:22 -0700

    unsafe_unused_lifetime.rs: Add help messages.

--- tests/compile-fail/unsafe_unused_lifetime.rs
+++ tests/compile-fail/unsafe_unused_lifetime.rs
@@ -1,5 +1,7 @@
 pub struct RefAndBool<'a, T: 'a> {
     //~^ ERROR: parameter `'a` is never used
     //~| ERROR: parameter `T` is never used
+    //~| HELP: consider removing `T` or using a marker such as `std::marker::PhantomData`
+    //~| HELP: consider removing `'a` or using a marker such as `std::marker::PhantomData`
     ptr_and_bit: usize
 }
