077836bcac0da885a24a8cdcde7c34bb93f4fbd1
Jim Blandy <jimb@red-bean.com>
2019-06-30 14:14:07 -0700

    antisamples: all `iterators_` tests pass.

--- tests/compile-fail/iterators_unused.rs
+++ tests/compile-fail/iterators_unused.rs
@@ -1,9 +1,10 @@
 // Rust warns about unused iterators.
-//
-// error-pattern: which must be used: iterator adaptors are lazy and do nothing unless consumed
 
 #[deny(unused_must_use)]
+//~^ NOTE: lint level defined here
 fn main() {
     ["earth", "water", "air", "fire"]
         .iter().map(|elt| println!("{}", elt));
+    //~^^ ERROR: unused `std::iter::Map` that must be used
+    //~| NOTE: iterators are lazy and do nothing unless consumed
 }
