f0c26c7339616a926b0d042dae702ce884bccdad
Jim Blandy <jimb@red-bean.com>
2019-07-01 16:08:02 -0700

    antisamples: all `traits_` tests pass, except for #410.

--- tests/compile-fail/traits_generic_dot_3.rs
+++ tests/compile-fail/traits_generic_dot_3.rs
@@ -7,8 +7,10 @@ fn dot<N: Add<Output=N> + Mul<Output=N> + Default>(v1: &[N], v2: &[N]) -> N
         total = total + v1[i] * v2[i];
         //~^ ERROR: cannot move out of type `[N]`, a non-copy slice
         //~| NOTE: cannot move out of here
+        //~| NOTE: move occurs because `v1[_]` has type `N`, which does not implement the `Copy` trait
         //~| ERROR: cannot move out of type `[N]`, a non-copy slice
         //~| NOTE: cannot move out of here
+        //~| NOTE: move occurs because `v2[_]` has type `N`, which does not implement the `Copy` trait
     }
     total
 }
