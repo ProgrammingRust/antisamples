425ac079e5e19625a5460faa5517d8472223e81e
Jim Blandy <jimb@red-bean.com>
2019-07-04 23:18:06 -0700

    Update compile-fail test text to match what we want in the book.

--- tests/compile-fail/enums_matching_moves_values.rs
+++ tests/compile-fail/enums_matching_moves_values.rs
@@ -19,8 +19,7 @@ fn main() {
         //~^ NOTE: value moved here
             ui.greet(&name, &language);
             ui.show_settings(&account);  // error: borrow of moved value: `account`
-                          // ^^^^^^^^ value borrowed here after partial move
-            //~^^ ERROR: borrow of moved value: `account`
+            //~^ ERROR: borrow of moved value: `account`
             //~| NOTE: value borrowed here after partial move
             //~| NOTE: move occurs because `account.language` has type `std::string::String`, which does not implement the `Copy` trait
         }
